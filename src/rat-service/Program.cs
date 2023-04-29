c01f1472478b6c993d1851e01525ed4a9ddaf179
Franklin Stennett <franklinstennett@gmail.com>
2023-04-29 16:24:35 -0600

    209 Added GCP client lib

--- src/rat-service/Program.cs
+++ src/rat-service/Program.cs
@@ -1,3 +1,6 @@
+using rat_service_core.Interfaces;
+using rat_service_infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICloudStorageClient, GoogleCloudStorageClient>();
 
 var app = builder.Build();
 
